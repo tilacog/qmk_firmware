4bec008bcd9a46df22c16d1e45ec626b1e4f2466
tilacog <tilacog@protonmail.com>
2020-07-31 10:14:16 -0300

    fix missing mouse layer access

--- keyboards/redox/keymaps/tilacog-workman/keymap.c
+++ keyboards/redox/keymaps/tilacog-workman/keymap.c
@@ -5,27 +5,26 @@
 #define _MOUSE 2
 
 // Shortcut to make keymap more readable
-#define KC_TABOS LGUI_T(KC_TAB)     // Tap: Tab        , hold: Super
-#define KC_BSSL LT(_SYMB, KC_BSPC)  // Tap: Backspace  , hold: Symbol layer
-#define KC_LSHM LSFT_T(KC_HOME)     // Tap: Home       , hold: Left Shift
-#define KC_LASP LALT_T(KC_SPC)      // Tap: Space      , hold: Left Alt
-#define KC_RASP RALT_T(KC_SPC)      // Tap: Space      , hold: Right Alt
-#define KC_PUSL LT(_SYMB, KC_PGUP)  // Tap: Page Up    , hold: Symbol layer
-#define KC_PDSL LT(_SYMB, KC_PGDN)  // Tap: Page Down  , hold: Symbol layer
-#define KC_TBSL LT(_SYMB, KC_TAB)   // Tap: Tab        , hold: Symbol layer
-#define KC_UNSL LT(_SYMB, KC_UNDS)  // Tap: Underscore , hold: Symbol layer
-#define KC_ENSL LT(_SYMB, KC_END)   // Tap: End        , hold: Symbol layer
-#define KC_GESL LT(_SYMB, KC_GESC)  // Tap: Esc-Grave  , hold: Symbol layer
+#define KC_TABOS LGUI_T(KC_TAB)      // Tap: Tab        , hold: Super
+#define KC_BMSL LT(_MOUSE, KC_BSPC)  // Tap: Backspace  , hold: Mouse layer
+#define KC_LSHM LSFT_T(KC_HOME)      // Tap: Home       , hold: Left Shift
+#define KC_LASP LALT_T(KC_SPC)       // Tap: Space      , hold: Left Alt
+#define KC_RASP RALT_T(KC_SPC)       // Tap: Space      , hold: Right Alt
+#define KC_PUSL LT(_SYMB, KC_PGUP)   // Tap: Page Up    , hold: Symbol layer
+#define KC_PDSL LT(_SYMB, KC_PGDN)   // Tap: Page Down  , hold: Symbol layer
+#define KC_TBSL LT(_SYMB, KC_TAB)    // Tap: Tab        , hold: Symbol layer
+#define KC_UNSL LT(_SYMB, KC_UNDS)   // Tap: Underscore , hold: Symbol layer
+#define KC_ENSL LT(_SYMB, KC_END)    // Tap: End        , hold: Symbol layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {  // clang-format off
 
   [_WORKMAN] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GESL ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,
+     KC_GESC ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TABOS,KC_Q    ,KC_D    ,KC_R    ,KC_W    ,KC_B    ,KC_BSPC ,                          KC_DEL  ,KC_J    ,KC_F    ,KC_U    ,KC_P    ,KC_SCLN ,KC_EQL  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_BSSL ,KC_A    ,KC_S    ,KC_H    ,KC_T    ,KC_G    ,KC_LBRC ,                          KC_RBRC, KC_Y    ,KC_N    ,KC_E    ,KC_O    ,KC_I    ,KC_QUOT ,
+     KC_BMSL ,KC_A    ,KC_S    ,KC_H    ,KC_T    ,KC_G    ,KC_LBRC ,                          KC_RBRC, KC_Y    ,KC_N    ,KC_E    ,KC_O    ,KC_I    ,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSHM ,KC_Z    ,KC_X    ,KC_M    ,KC_C    ,KC_V    ,KC_PDSL ,KC_PDSL ,        KC_TBSL ,KC_UNSL ,KC_K    ,KC_L    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_BSLS ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
